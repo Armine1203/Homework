fb172f0708d2eb5f962fbc4f9f2ddb609c8fe61c
Armine1203 <arminelalayan2003@gmail.com>
2024-09-04 22:53:22 +0400

    Homework

---
+++ Problem1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+
+        int number = scanner.nextInt();
+        System.out.println("number " +number );
+
+        int thousandPart = number / 1000;
+        System.out.println("thousand part is "+ thousandPart);
+
+        int hundredPart = (number / 100) % 10;
+        System.out.println("hundred part is "+hundredPart);
+
+        int tenPart = (number / 10) % 10;
+        System.out.println("tenth part is "+tenPart);
+
+        int unitPart = number % 10 ;
+        System.out.println("unit part is "+unitPart);
+
+
+     //
+    }
+}
