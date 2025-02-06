552ad870d516bb23fe82804d79892ee4725c365d
Armine1203 <arminelalayan2003@gmail.com>
2025-02-06 17:28:55 +0400

    Last changes

--- hw22_3D_Point/Point3D.java
+++ hw22_3D_Point/Point3D.java
@@ -2,7 +2,7 @@ package hw22_3D_Point;
 
 import java.util.Objects;
 
-public class Point3D {
+public class Point3D implements Comparable<Point3D> {
     private final int x;
     private final int y;
     private final int z;
@@ -24,7 +24,7 @@ public class Point3D {
         return x == point3D.x
                 && y == point3D.y
                 && z == point3D.z
-                && Objects.equals(color, point3D.color)
+                && Objects.equals(color, point3D.color)//սովորական իքվալզ կանչել
                 && Objects.equals(pointName, point3D.pointName);
     }
 
@@ -38,6 +38,14 @@ public class Point3D {
         return " Point3D{" +
                 x + " " + y + " " + z + " " + color + " " + pointName +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Point3D o) {
+        if (this.x != o.x) return x - o.x;
+        if (this.y != o.y) return y - o.y;
+        if (this.z != o.z) return z - o.z;
+        return this.color.compareTo(o.color);
     }
 
 }
--- hw22_3D_Point/PointDemo.java
+++ hw22_3D_Point/PointDemo.java
@@ -1,35 +1,36 @@
 package hw22_3D_Point;
-
 import java.util.*;
 
 public class PointDemo {
     public static void main(String[] args) {
         //--------------------------First part----------------------------------
-        Map<Creator, Point3D> hashMapOfPoints = new HashMap<>();
+        Map<Point3D, Creator> hashMapOfPoints = new HashMap<>();
 
-        Point3D point1 = new Point3D(2, 1, 5, "green", "point1");//are different via point name
-        Point3D point2 = new Point3D(2, 1, 5, "green", "point2");//are different via point name
-        Point3D point3 = new Point3D(5, 6, 7, "red", "point3");
-        Point3D point4 = new Point3D(8, 9, 10, "blue", "point4");
-        Point3D point5 = new Point3D(2, 12, 11, "black", "point5");
-        Point3D point6 = new Point3D(20, 41, 50, "yellow", "point6");
+        Point3D point1 = new Point3D(2,1,5,"green","point1");//are different via point name
+        Point3D point2 = new Point3D(2,1,5,"green","point2");//are different via point name
+        Point3D point3 = new Point3D(5,6,7,"red","point3");
+        Point3D point4 = new Point3D(8,9,10,"blue","point4");
+        Point3D point5 = new Point3D(2,12,11,"black","point5");
+        Point3D point6 = new Point3D(20,41,50,"yellow","point6");
 
-        hashMapOfPoints.put(new Creator("Ann"), point1);
-        hashMapOfPoints.put(new Creator("Bob"), point2);
-        hashMapOfPoints.put(new Creator("Alice"), point3);
-        hashMapOfPoints.put(new Creator("Doe"), point4);
-        hashMapOfPoints.put(new Creator("Ann2"), point5);
-        hashMapOfPoints.put(new Creator("Bob2"), point6);
+        hashMapOfPoints.put(point1, new Creator("Ann"));
+        hashMapOfPoints.put(point2, new Creator("Bob"));
+        hashMapOfPoints.put(point3, new Creator("Alice"));
+        hashMapOfPoints.put(point4, new Creator("Doe"));
+        hashMapOfPoints.put(point5, new Creator("Ann2"));
+        hashMapOfPoints.put(point6, new Creator("Bob2"));
 
-        System.out.println(hashMapOfPoints + "\n");
+        System.out.println(hashMapOfPoints+"\n");
+        System.out.println(hashMapOfPoints.size());
 
-        Map<Creator, Point3D> treeMap = new TreeMap<Creator, Point3D>(hashMapOfPoints);
-        System.out.println(treeMap + "\n");
+        Map<Point3D, Creator> treeMap = new TreeMap<Point3D, Creator>(hashMapOfPoints);
+        System.out.println(treeMap+"\n");
+        System.out.println(treeMap.size());
 
 //--------------------------Second part----------------------------------
-        System.out.println(swapMap(treeMap) + "\n");//????????????????
+        System.out.println(swapMap(treeMap)+"\n");//
 
-        System.out.println(swapMap(hashMapOfPoints) + "\n");//????????????????????????????/
+        System.out.println(swapMap(hashMapOfPoints)+"\n");
 
 //--------------------------Third part----------------------------------
         List<Integer> arrayList = new ArrayList<>();
@@ -45,25 +46,24 @@ public class PointDemo {
 
         System.out.println(countOfDuplicates(arrayList));
     }
-
-    public static <K, V> Map<V, K> swapMap(Map<K, V> map) {
-        Map<V, K> swappedMap = new HashMap<>();
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            swappedMap.put(entry.getValue(), entry.getKey());
+    public static <K,V> Map<V,K> swapMap(Map<K,V> map){
+        Map<V,K> swappedMap = new HashMap<>();
+        for (Map.Entry<K,V> entry : map.entrySet()) {
+            swappedMap.put(entry.getValue(),entry.getKey());
         }
         return swappedMap;
     }
 
 
-    public static Map<Integer, Integer> countOfDuplicates(List<Integer> list) {
-        Map<Integer, Integer> mapOfData = new HashMap<>();
+    public static Map<Integer, Integer> countOfDuplicates(List<Integer> list){
+        Map<Integer,Integer> mapOfData = new HashMap<>();
 
-        for (int item : list) {
-            if (mapOfData.containsKey(item)) {
+        for (int item: list) {
+            if (mapOfData.containsKey(item)){
                 int count = mapOfData.get(item);
-                mapOfData.put(item, count + 1);
-            } else {
-                mapOfData.put(item, 1);
+                mapOfData.put(item, count+1);
+            }else {
+                mapOfData.put(item,1);
             }
 
         }
